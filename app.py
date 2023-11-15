52849f91da0fa39c05d9b42de4e88f3f1d99a178
Luke <luke.manongsong.cics@ust.edu.ph>
2023-11-15 09:48:43 +0800

    Now uses GPT-4-turbo

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 import os
 from dotenv import load_dotenv
 from langchain import PromptTemplate, OpenAI, LLMChain
+from typing import Dict, List
 from langchain.chat_models import ChatOpenAI
-from langchain.chains.conversation.memory import ConversationBufferWindowMemory
+from langchain.chains.conversation.memory import ConversationBufferWindowMemory, ConversationBufferMemory
 from langchain.tools import BaseTool, DuckDuckGoSearchRun
 from langchain.agents import initialize_agent, AgentType, AgentExecutor, Tool
 from langchain.agents.agent_toolkits.jira.toolkit import JiraToolkit
+from langchain.agents.agent_toolkits.base import BaseToolkit, BaseTool
 from langchain.utilities.jira import JiraAPIWrapper
+from langchain.tools.jira.tool import JiraAction
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.chains import RetrievalQA, LLMMathChain
@@ -19,9 +22,8 @@ DB_FAISS_PATH = 'vectorstore/db_faiss'
 
 custom_prompt_template = """You are a customer service assistant. 
 Use the following pieces of information to answer the user's question.
-If you don't know the answer, please just say that you don't know the answer, don't try to make up 
-an answer. For more complex issues that require assistance or follow-up, suggest to the user that you can help them create support tickets.
-If it's a greeting, just greet back politely.
+If you don't know the answer, just say that you don't know the answer, don't try to make up 
+an answer. For more complex issues that require assistance or follow-up, suggest to the user that you can help them create a support ticket.
 
 Context: {context}
 Question: {question}
@@ -37,6 +39,7 @@ def set_custom_prompt():
     prompt = PromptTemplate(template=custom_prompt_template, input_variables=['context', 'question'])
 
     return prompt
+
 
 #Retrieval QA Chain
 def retrieval_qa_chain(llm, prompt, db):
@@ -80,11 +83,11 @@ llm_math_chain = LLMMathChain.from_llm(llm=load_llm(), verbose=True)
 class HumanInputChainlit(BaseTool):
     """Tool that adds the capability to ask user for input."""
 
-    name = "human"
+    name = "AskHuman"
     description = (
-        "You can ask a human for guidance when you think you "
-        "got stuck or you are not sure what to do next. "
-        "The input should be a question for the human."
+        "You can ask the customer for clarifications when you think you "
+        "need to know more. "
+        "The input should be a question for the customer."
         "Only use when you need clarification"
     )
 
@@ -106,7 +109,42 @@ class HumanInputChainlit(BaseTool):
         """Use the Human input tool."""
         res = await cl.AskUserMessage(content=query).send()
         return res["content"]
+    
 
+class JiraToolkit(BaseToolkit):
+    """Jira Toolkit."""
+
+    tools: List[BaseTool] = []
+
+    @classmethod
+    def from_jira_api_wrapper(cls, jira_api_wrapper: JiraAPIWrapper) -> "JiraToolkit":
+        operations: List[Dict] = [
+            {
+                "mode": "create_issue",
+                "name": "Create Issue",
+                "description": """
+                    This tool is a wrapper around atlassian-python-api's Jira issue_create API, useful when you need to create a Jira issue.
+                    The input to this tool is a dictionary specifying the fields of the Jira issue, and will be passed into atlassian-python-api's Jira `issue_create` function.
+                    For example, to create a low priority task called "Unable to login" with description "I am unable to log in to my AWS console.", you would pass in the following dictionary: 
+                    {{"summary": "Unable to login", "description": "Unable to log in to my AWS console.", "issuetype": {{"name": "Submit a request or incident"}}, "priority": {{"name": "Low"}}, "project": {{"key": "SD"}}}}
+                    After creating an issue, provide the link of the issue to allow status tracking, Example: https://chatsight-vsd.atlassian.net/browse/SD-27
+                    """
+            }
+        ]
+        tools = [
+            JiraAction(
+                name=action["name"],
+                description=action["description"],
+                mode=action["mode"],
+                api_wrapper=jira_api_wrapper,
+            )
+            for action in operations
+        ]
+        return cls(tools=tools)
+
+    def get_tools(self) -> List[BaseTool]:
+        """Get the tools in the toolkit."""
+        return self.tools
 
 ##Chainlit code
 @cl.on_chat_start
@@ -116,32 +154,61 @@ def start():
     jira = JiraAPIWrapper()
     jira_toolkit = JiraToolkit.from_jira_api_wrapper(jira)
 
+
     tools = [
         Tool(
             name="Knowledge Retrieval",
             func=qa_bot(),
-            description="useful when you need to answer questions about AWS."
-        ), HumanInputChainlit()
+            description="useful when you need to answer customer queries."
+        ), 
+        HumanInputChainlit()
     ] + jira_toolkit.get_tools()
 
 
-    memory = ConversationBufferWindowMemory(
-        memory_key='chat_history',
-        k=3,
+    memory = ConversationBufferMemory(
+        memory_key='history',
         return_messages=True
     )
+
+
+    PREFIX = """"You are a customer service assistant. Answer questions politely.
+    Look for solutions first using Knowledge Retrieval.
+    After answering a question, ask the customer if they found the response helpful. 
+    If you've used Knowledge Retrieval and still don't know the answer, ask customer if they want to create a support ticket.
+    If you're going to create a ticket, ask customer about the details of the issue and possible error messages.
+    Use knowledge retrieval first to check if there is a solution before thinking about creating a ticket.
+    """
+
+    FORMAT_INSTRUCTIONS = """Use the following format:
+    Question: the input question you must answer
+    Thought: you should always think about what to do
+    Action: the action to take, should be one of [{tool_names}]
+    Action Input: the input to the action
+    Observation: the result of the action
+    ... (this Thought/Action/Action Input/Observation can repeat N times)
+    Thought: I now know the final answer
+    Final Answer: the final answer to the original input question"""
+    SUFFIX = """
+    Begin!
+
+    Question: {input}
+    Thought:{agent_scratchpad}"""
 
     conversational_agent = initialize_agent(
         agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
         tools=tools,
-        llm=ChatOpenAI(temperature=0, model="gpt-3.5-turbo"),
+        llm=ChatOpenAI(temperature=0, model="gpt-4-1106-preview"),
         verbose=True,
-        max_iterations=10,
+        max_iterations=4,
         early_stopping_method='generate',
         memory=memory,
-        handle_parsing_errors=True
+        handle_parsing_errors=True,
+        agent_kwargs={
+            'prefix':PREFIX,
+            'format_instructions':FORMAT_INSTRUCTIONS,
+            'suffix':SUFFIX
+    }
     )
-
     cl.user_session.set("agent", conversational_agent)
 
 
@@ -149,13 +216,7 @@ def start():
 @cl.on_message
 async def main(message: cl.Message):
     agent = cl.user_session.get("agent")  # type: AgentExecutor
-    prompt = """Context: You are a customer service assistant.
-        Task: You will be asked questions about AWS and you will answer them. You may also be asked to create an issue, and you will guide me on the details the I need to provide: a short description of the issue and priority level(low, medium, high).
-        Short description and priority level must always be provided before creation of issue if not already provided. Ask customer first using HumanInputChain tool before creating an issue.
-        For create issue, always create issues from project Service Desk with the project key of SD. The issue type is 'Submit a request or incident'. Project key is "SD".
-        Make sure to include Project key "SD" when creating an issue.
-        Input:"""
-    res = await agent.run(prompt +
-        message
+    res = await agent.arun(
+        message, callbacks=[cl.AsyncLangchainCallbackHandler()]
     )
     await cl.Message(content=res).send()
