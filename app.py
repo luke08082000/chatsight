e330e1605d89ed6588abfd333ce32da673d13430
Luke <luke.manongsong.cics@ust.edu.ph>
2023-11-25 22:10:20 +0800

    Updated Self-healing toosl

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from typing import Dict, List
 from langchain.chat_models import ChatOpenAI
 from langchain.chains.conversation.memory import ConversationBufferWindowMemory, ConversationBufferMemory
 from langchain.tools import BaseTool, DuckDuckGoSearchRun
-from langchain.agents import initialize_agent, AgentType, AgentExecutor, Tool
+from langchain.agents import initialize_agent, AgentType, AgentExecutor, Tool, load_tools
 from langchain.agents.agent_toolkits.jira.toolkit import JiraToolkit
 from langchain.agents.agent_toolkits.base import BaseToolkit, BaseTool
 from langchain.utilities.jira import JiraAPIWrapper
@@ -15,6 +15,7 @@ from langchain.vectorstores import FAISS
 from langchain.chains import RetrievalQA, LLMMathChain
 from chainlit.sync import run_sync
 import chainlit as cl
+import boto3
 
 load_dotenv()
 
@@ -77,18 +78,15 @@ def qa_bot():
 
 jira = JiraAPIWrapper()
 jira_toolkit = JiraToolkit.from_jira_api_wrapper(jira)
-search = DuckDuckGoSearchRun()
-llm_math_chain = LLMMathChain.from_llm(llm=load_llm(), verbose=True)
 
 class HumanInputChainlit(BaseTool):
     """Tool that adds the capability to ask user for input."""
 
     name = "AskHuman"
     description = (
-        "You can ask the customer for clarifications when you think you "
-        "need to know more. "
-        "The input should be a question for the customer."
-        "Only use when you need clarification"
+        """You can ask the customer for clarifications when you think you need to know more. 
+        The input should be a question for the customer.
+        Use this to ask customer's instance id"""
     )
 
     def _run(
@@ -98,7 +96,7 @@ class HumanInputChainlit(BaseTool):
     ) -> str:
         """Use the Human input tool."""
 
-        res = run_sync(cl.AskUserMessage(content=query).send())
+        res = run_sync(cl.AskUserMessage(content=query, timeout=300).send())
         return res["content"]
 
     async def _arun(
@@ -128,6 +126,18 @@ class JiraToolkit(BaseToolkit):
                     For example, to create a low priority task called "Unable to login" with description "I am unable to log in to my AWS console.", you would pass in the following dictionary: 
                     {{"summary": "Unable to login", "description": "Unable to log in to my AWS console.", "issuetype": {{"name": "Submit a request or incident"}}, "priority": {{"name": "Low"}}, "project": {{"key": "SD"}}}}
                     After creating an issue, provide the link of the issue to allow status tracking, Example: https://chatsight-vsd.atlassian.net/browse/SD-27
+                    Include your observation of the log into the description.
+                    """
+            },
+                        {
+                "mode": "update_status",
+                "name": "Update Issue Status",
+                "description": """
+                    This tool is a wrapper around atlassian-python-api's Jira update_status API, useful when you need to update a status of a Jira issue.
+                    The input to this tool is a dictionary specifying the fields of the Jira issue, and will be passed into atlassian-python-api's Jira `update_status` function.
+                    Update status to "In Progress" when used for self-healing and then update again to "Done" after self-healing.
+                    For example, to update an issue status of an issue with an id of "SD-19" to "In Progress" you would pass in the following: 
+                    {{"issue_key": "SD-19", "status": "In Progress"}}
                     """
             }
         ]
@@ -146,11 +156,48 @@ class JiraToolkit(BaseToolkit):
         """Get the tools in the toolkit."""
         return self.tools
 
+#Self-healing - reboot ec2 instance 
+import time
+
+def reboot_ec2_instance(instance_id):
+    ec2 = boto3.client('ec2', region_name="ap-southeast-2")
+    
+    # Initiating the reboot
+    ec2.reboot_instances(InstanceIds=[instance_id])
+    
+    # Wait for a short duration to allow the reboot to take effect
+    time.sleep(5)
+    
+    # Check the instance state after the reboot
+    instance = ec2.describe_instances(InstanceIds=[instance_id])
+    
+    # Extract the state information
+    state = instance['Reservations'][0]['Instances'][0]['State']['Name']
+    
+    # Validate if the state is 'running,' indicating a successful reboot
+    if state == 'running':
+        return f"EC2 instance {instance_id} successfully rebooted."
+    else:
+        return f"Failed to reboot EC2 instance {instance_id}. Current state: {state}"
+    
+#Retrieve EC2 Instance Console Output
+def get_console_output(instance_id):
+    ec2 = boto3.client('ec2')
+    response = ec2.get_console_output(InstanceId=instance_id)
+
+    return response['Output']
+
+#Check EC2 Instance Status
+def check_instance_status(instance_ids):
+    ec2 = boto3.client('ec2')
+    response = ec2.describe_instance_status(InstanceIds=[instance_ids])
+    
+    for status in response['InstanceStatuses']:
+        return(f"Instance ID: {status['InstanceId']}, Status: {status['InstanceState']['Name']}")
+
 ##Chainlit code
 @cl.on_chat_start
 def start():
-    llm = ChatOpenAI(temperature=0, streaming=True)
-    llm1 = OpenAI(temperature=0, streaming=True)
     jira = JiraAPIWrapper()
     jira_toolkit = JiraToolkit.from_jira_api_wrapper(jira)
 
@@ -159,8 +206,34 @@ def start():
         Tool(
             name="Knowledge Retrieval",
             func=qa_bot(),
-            description="useful when you need to answer customer queries."
-        ), 
+            description="""Useful when you need to answer customer queries and troubleshoot issues."""
+        ),
+        Tool(
+            name="Reboot EC2 Instance",
+            func=reboot_ec2_instance,
+            description="""Only call after instance id has been collected.
+            This is one of the self-healing/troubleshooting tools. 
+            This reboots an EC2 instance when run. Useful when customer complains about Instance Performance Issues.
+            Pass the instance id as the action input.
+            """
+        ),
+        Tool(
+            name="Get EC2 Console Output",
+            func=get_console_output,
+            description="""Only call after instance id has been collected.
+            This is one of the self-healing/troubleshooting tools.
+            This is used to retrieve console output logs of an EC2 instance.
+            Useful for diagnosing boot issues, error messages during startup, or other console-related problems.
+            Pass the instance id as the action input."""
+        ),
+        Tool(
+            name="Check EC2 Instance Status",
+            func=check_instance_status,
+            description="""Only call after instance id has been collected.
+            This is one of the self-healing/troubleshooting tools.
+            This is used to check the status of an EC2 instance.
+            Pass the instance id as the action input."""
+        ),
         HumanInputChainlit()
     ] + jira_toolkit.get_tools()
 
@@ -177,6 +250,13 @@ def start():
     If you've used Knowledge Retrieval and still don't know the answer, ask customer if they want to create a support ticket.
     If you're going to create a ticket, ask customer about the details of the issue and possible error messages.
     Use knowledge retrieval first to check if there is a solution before thinking about creating a ticket.
+
+    There are the scenarios that can happen: 
+    1. The customer asks an FAQ and you will answer using Knowledge Retrieval.
+    2. The customer reports an issue, you will ask the details of the issue (AskHuman) and create an issue (Create Issue).
+    3. The customer reports an issue that can be solved using one of the self-healing tools (Reboot EC2 Instance, Get EC2 Console Output, Check EC2 Instance Status).
+    In this case, first create a ticket (Create Issue), then run the self-healing tool, then ask the customer if it solved the problem (AskHuman),
+    then lastly, Update Issue Status to "Done" if problem was solved, if not Update Issue Status to "Pending".
     """
 
     FORMAT_INSTRUCTIONS = """Use the following format:
@@ -194,12 +274,12 @@ def start():
     Question: {input}
     Thought:{agent_scratchpad}"""
 
-    conversational_agent = initialize_agent(
+    agent = initialize_agent(
         agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
         tools=tools,
         llm=ChatOpenAI(temperature=0, model="gpt-4-1106-preview"),
         verbose=True,
-        max_iterations=4,
+        max_iterations=8,
         early_stopping_method='generate',
         memory=memory,
         handle_parsing_errors=True,
@@ -209,7 +289,7 @@ def start():
             'suffix':SUFFIX
     }
     )
-    cl.user_session.set("agent", conversational_agent)
+    cl.user_session.set("agent", agent)
 
 
 
